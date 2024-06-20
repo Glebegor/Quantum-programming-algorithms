7f06d0b502a99431d687e5146fdd8f8ab42d87b6
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-06-21 00:38:55 +0200

    Created basic layout.

---
+++ main.py
@@ -0,0 +1,10 @@
+import qiskit
+import qiskit_ibm_runtime
+import qiskit_aer
+import numpy as np
+import random as rand
+
+
+version = qiskit.__version__
+print("The version of Qiskit is -", version)
+
