49e07a4fb45fb701981a34256cabd32d4a28435c
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-06-23 22:52:43 +0200

    Created logic of the circuit draw and build.

--- testquantum.py
+++ testquantum.py
@@ -1,9 +1,11 @@
+import time
+
 import qiskit
 import qiskit_ibm_runtime
 import qiskit_aer
 import numpy as np
 import random as rand
-
+import os
 
 
 version = qiskit.__version__
@@ -28,7 +30,6 @@ circuit.measure(quantumReg, classicalReg)
 
 print(":)")
 
-circuit.draw(output='mpl', filename=imageFolder + 'test.png')
 
 # execute the circuit 1024 times
 job = qiskit_aer.AerSimulator(method='automatic').run(circuit,shots=1024)
@@ -44,28 +45,27 @@ print(counts)
 
 
 # Projecting Quantum class
-class GatesStack:
-    def __init__(self):
-        self.stack = list()
 
 class Quantum:
-    def __init__(self, gStack, qbCount, imgFolder):
+    def __init__(self, gStack, qbCount):
         self.qiskitVersion = qiskit.__version__
-        self.imagesPath = imgFolder # Folder to Images
+        self.imagesPath = "./Images" # Folder to Images
+        self.loggerPath = "./Logger" # Folder to Loggs
 
         self.gatesStack = gStack # Stack of the gates
         self.qubitsCount = qbCount # count of the qubits
 
         # Registers
-        self.regQ = qiskit.QuantumRegister(self.qubitsCount, "QuantumRegister") # Quantum
-        self.regC = qiskit.ClassicalRegister(self.qubitsCount, "ClassicRegister") # Classic
+        self.regQ = qiskit.QuantumRegister(self.qubitsCount, "QRegister") # Quantum
+        self.regC = qiskit.ClassicalRegister(self.qubitsCount, "CRegister") # Classic
 
         self.circuit = qiskit.QuantumCircuit(self.regQ, self.regC) # Circuit
 
         self.gates = ["h", "x", "z", "ry", "cx", "cu3", "ccx"]
-
-    def saveImage(self):
-        pass
+    def findId(self):
+        return time.ctime().replace(" ", "_").replace(":", "-")
+    def saveImage(self, name):
+        self.circuit.draw(output='mpl', filename=self.imagesPath + "/" + name)
 
     def loggCircuit(self):
         pass
@@ -82,49 +82,49 @@ class Quantum:
 
         if (gate == "h"):
             if len(gateElement) != 2:
-                print("Gate h has 1 parametr")
+                print("Gate h has 1 param.")
                 return
 
             self.isInCircuit(gate[1:])
             self.circuit.h(self.regQ[gateElement[1]]) # ("h", 1,)
         elif (gate == "x"):
             if len(gateElement) != 2:
-                print("Gate x has 1 parametr")
+                print("Gate x has 1 param.")
                 return
 
             self.isInCircuit(gate[1:])
             self.circuit.h(self.regQ[gateElement[1]]) # ("x", 1,)
         elif (gate == "z"):
             if len(gateElement) != 2:
-                print("Gate z has 1 parametr")
+                print("Gate z has 1 param.")
                 return
 
             self.isInCircuit(gate[1:])
             self.circuit.h(self.regQ[gateElement[1]]) # ("z", 1,)
         elif (gate == "ry"):
             if len(gateElement) != 3:
-                print("Gate ry has 2 parametr")
+                print("Gate ry has 2 params.")
                 return
 
             self.isInCircuit(gate[1:2])
             self.circuit.ry(2*gateElement[2], self.regQ[gateElement[1]]) # ("ry", 1, 45)
         elif (gate == "cx"):
             if len(gateElement) != 2:
-                print("Gate cx has 2 parametr")
+                print("Gate cx has 2 params.")
                 return
 
             self.isInCircuit(gate[1:])
             self.circuit.cx(self.regQ[gateElement[1]], self.regQ[gateElement[2]]) # ("cx", 1, 2)
         elif (gate == "cu3"):
             if len(gateElement) != 4:
-                print("Gate cu3 has 3 parametr")
+                print("Gate cu3 has 3 params.")
                 return
 
             self.isInCircuit(gate[1:3])
             self.circuit.cu3(self.regQ[gateElement[3]], 0, 0, self.regQ[gateElement[1]], self.regQ[gateElement[2]]) # ("cu3", 1, 2, 45)
         elif (gate == "ccx"):
             if len(gateElement) != 4:
-                print("Gate ccx has 3 parametr")
+                print("Gate ccx has 3 params.")
                 return
 
             self.isInCircuit(gate[1:])
@@ -141,8 +141,18 @@ class Quantum:
                 return
             else:
                 print("Operator - " + result + " - used.")
+        self.build()
+    def build(self):
+        self.circuit.measure(self.regQ, self.regC)
+        print("Circuit was measured.")
 
-stackOfGates = [["h", 1],["r", 2],["h", 3],["h", 4]]
-quantum = Quantum(stackOfGates, 10, "./Images")
+        idOfJob = self.findId()
+        print("Created job: " + idOfJob)
+
+        self.saveImage(idOfJob)
+
+
+stackOfGates = [["h", 1],["h", 2],["h", 3],["h", 4]]
+quantum = Quantum(stackOfGates, 10)
 
 quantum.run()
