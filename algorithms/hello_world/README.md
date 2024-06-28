a3fe7e4e860cffacb0720f02844f592424f52472
Hlib Arseniuk <85109823+Glebegor@users.noreply.github.com>
2024-06-28 20:34:26 +0200

    Created hello world on qiskit. Fixed bugs.

---
+++ algorithms/hello_world/README.md
@@ -0,0 +1,4 @@
+# Hello world algorithm
+This is a simple quantum circuit that creates a superposition of 2 qubits and measures them.
+
+![Circuit diagram](./circuit.png)
--- algorithms/hello_world/main.py
+++ algorithms/hello_world/main.py
@@ -1,35 +1,77 @@
 import numpy as np
 import random as rand
+import dotenv
+import os
 
 import qiskit as qk
 import qiskit_ibm_runtime as qk_runtime
 import qiskit_aer as qk_aer
 
 '''
-Created by Hlib Arseniuk 2024.
+Created by Hlib Arseniuk (Glebegor) 2024.
 Application for Quantum Computing.
 Hello world.
 Qubits: 2 
 '''
 
-def saveImage(circuit, name):
+def saveImage(circuit: qk.circuit.QuantumCircuit, name: str) -> None:
+    '''
+    Save image of circuit.
+    '''
     circuit.draw(output='mpl', filename=name)
 
-def runSimulator() -> None:
+def runSimulator(circuit: qk.circuit.QuantumCircuit, shorts_count: int) -> None:
+    '''
+    Run simulator.
+    '''
+    job = qk_aer.AerSimulator(method='automatic').run(circuit, shots=shorts_count)
+    counts = job.result().get_counts(circuit)
+    print(counts)
+
+def runIBM(circuit: qk.circuit.QuantumCircuit, shorts_count: int) -> None:
+    '''
+    Run IBM backend.
+    '''
+
+    qk_runtime.QiskitRuntimeService.save_account(
+            channel="ibm_quantum",
+            name=os.getenv("IBM_NAME"),
+            token=os.getenv("IBM_TOKEN"),
+            set_as_default=True
+    )
+
     pass
-def runIBM() -> None:
-    pass
+
+def algorithm(qb_count: int) -> qk.circuit.QuantumCircuit:
+    '''
+    Algorithm Hello World on qiskit.
+    Use 2 qubits.
+    Hadamar gate and C-NOT gate.
+    '''
+    regQuantum = qk.QuantumRegister(qb_count, "q_reg")
+    regClassic = qk.ClassicalRegister(qb_count, "c_reg")
+
+    circuit = qk.QuantumCircuit(regQuantum, regClassic)
+
+    circuit.h(regQuantum[0])
+    circuit.cx(regQuantum[0], regQuantum[1])
+
+    circuit.measure(regQuantum, regClassic)
+    return circuit
 
 def main() -> None:
-    run = int(input("Choose simulator: 1. IBM or 2. Aer"))
+    dotenv.load_dotenv()
 
+    run = int(input("Choose simulator: 1. IBM or 2. Aer: \n"))
+    shorts_count = int(input("Shorts count: \n"))
 
-
+    circuit = algorithm(2)
+    saveImage(circuit, "circuit")
 
     if run == 1:
-        runIBM()
+        runIBM(circuit, shorts_count)
     elif run == 2:
-        runSimulator()
+        runSimulator(circuit, shorts_count)
     else:
         print("Choose 1 or 2.")
 
